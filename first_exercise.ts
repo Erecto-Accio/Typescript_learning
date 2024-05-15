2a70d87bfbb2d46f15cf0b37d936066428bd713f
Thedarkness-bd <toisiftasrik@gmail.com>
2024-05-15 14:01:43 +0600

    First exercise added

---
+++ first_exercise.ts
@@ -0,0 +1,13 @@
+let greeting: string = "Guten Morgen!";
+greeting = greeting.toUpperCase();
+console.log(greeting);
+
+let number1: number = 19;
+let number2: number = 6;
+
+let sum: number = number1 + number2;
+console.log("Sum is: ", sum);
+
+let isAdult: boolean = number1 >= 18;
+isAdult = !isAdult;
+console.log(isAdult);
