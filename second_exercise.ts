065c348d312c86584b7146ac9eb99aff6c2a6544
Thedarkness-bd <toisiftasrik@gmail.com>
2024-05-15 15:13:25 +0600

    Second exercise added

---
+++ second_exercise.ts
@@ -0,0 +1,11 @@
+// 1
+let orderStatus: "processing" | "shipped" | "delivered" = "processing";
+
+orderStatus = "shipped";
+orderStatus = "delivered";
+console.log(orderStatus);
+
+// 2
+let discount: number | string = 20;
+discount = "20%";
+console.log(discount);
