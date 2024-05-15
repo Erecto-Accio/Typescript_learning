e60ec45fb45019b80af7e20a57b275d427ecbad0
Thedarkness-bd <toisiftasrik@gmail.com>
2024-05-15 15:18:35 +0600

    Array added

---
+++ array_deep_dive.ts
@@ -0,0 +1,21 @@
+let prices: number[] = [100, 75, 6, 56, 6];
+console.log(prices);
+
+let fruits: string[] = ["Apple", "pineapple", "Jackfruit", "Guava"];
+
+console.log(fruits);
+
+//random array
+let randomArray: any[] = ["hello", 1, 2, 3, false];
+console.log(randomArray);
+
+// empty array
+let emptyArray: boolean[] = [];
+console.log(emptyArray);
+
+// union array
+let names = ["peter", 1];
+console.log(names);
+
+let array: (string | boolean)[] = ["apple", true, "orange", false];
+console.log(array);
