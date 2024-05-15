44a5c3c67711bf992c0c92e16016a841bdef3568
Thedarkness-bd <toisiftasrik@gmail.com>
2024-05-15 13:51:50 +0600

    Type annotations

---
+++ second.ts
@@ -0,0 +1,8 @@
+let fullName: string = "Tousif Tasrik";
+console.log("Name: " + fullName);
+
+let age: number = 23;
+console.log("Age: ", age);
+
+let major: string = "Computer Science";
+console.log("Major: " + major);
