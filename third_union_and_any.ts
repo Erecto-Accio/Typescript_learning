a5f6fb350c25daa800b2a7b52524a3424910b7c7
Thedarkness-bd <toisiftasrik@gmail.com>
2024-05-15 15:05:23 +0600

    Union and any learned

---
+++ third_union_and_any.ts
@@ -0,0 +1,10 @@
+let num: number | string = 10;
+console.log(num);
+
+num = "$10";
+console.log(num);
+
+let notSure: any = 4; // in any anything can be stored
+notSure = "Hello,World";
+console.log(notSure);
+notSure = false;
